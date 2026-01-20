fbfa69a25de4d8e947face85656f616dce740b06
Nublado <nublado.dev@gmail.com>
2026-01-19 21:46:37 -0800

    ...

---
+++ tests/test_app_settings.py
@@ -0,0 +1,17 @@
+import pytest
+
+from django.conf import settings as django_settings
+
+from django_nublado_translation.conf import app_settings
+
+
+class TestAppSettings:
+    """
+    Tests for the blog app settins.
+    """
+
+    def test_app_settings_prefix(self):
+        assert app_settings.app_settings_prefix == "NUBLADO_TRANSLATION_"
+
+    def test_app_settings_values(self):
+        assert app_settings.NUBLADO_TRANSLATION_SOURCE_LANGUAGE == django_settings.LANGUAGE_CODE
