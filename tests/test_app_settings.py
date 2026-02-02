3ebb12c55f7649831693d2adf62a432e0344fc19
Nublado <nublado.dev@gmail.com>
2026-02-02 01:04:08 -0800

    ...

--- tests/test_app_settings.py
+++ tests/test_app_settings.py
@@ -1,20 +1,23 @@
-# import pytest
+import pytest
 
-# from django.conf import settings as django_settings
+from django.conf import settings as django_settings
 
-# from django_nublado_translation.conf.app_settings import SETTINGS_DICT_NAME, app_settings
-#
+from django_nublado_translation.conf.app_settings import SETTINGS_DICT_NAME, app_settings
 
-# class TestAppSettings:
-#     """
-#     Tests for the app settings.
-#     """
 
-#     def test_app_settings_prefix(self):
-#         assert SETTINGS_DICT_NAME == "DJANGO_NUBLADO_TRANSLATION"
+class TestAppSettings:
+    """
+    Tests for the app settings.
+    """
 
-#     def test_app_settings_values(self):
-#         assert (
-#             app_settings.SOURCE_LANGUAGE
-#             == django_settings.LANGUAGE_CODE
-#         )
+    def test_app_settings_dict_name(self):
+        """
+        Sanity check. Make sure app settings dict namme is the desired value.
+        """
+        assert SETTINGS_DICT_NAME == "DJANGO_NUBLADO_TRANSLATION"
+
+    def test_app_settings_values(self):
+        assert (
+            app_settings.SOURCE_LANGUAGE
+            == django_settings.LANGUAGE_CODE
+        )
